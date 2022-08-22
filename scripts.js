75673aada51482b0f39409ec5db7e00ceda71f9f
AndrewHemry <Andrewhemry@gmail.com>
2022-08-21 23:48:16 -0500

    updating hair salon

--- scripts.js
+++ scripts.js
@@ -1,6 +1,21 @@
-const placeForResults = document.getElementById("results")
+// const placeForResults = document.getElementById("results")
 
-new URLSearchParams(window.location.search).forEach((value, name) => {
-  placeForResults.append(`${name} : ${value}`)
-  placeForResults.append(document.createElement("br"))
+// new URLSearchParams(window.location.search).forEach((value, name) => {
+//   placeForResults.append(`${name} : ${value}`)
+//   placeForResults.append(document.createElement("br"))
+// })
+
+console.log("It is working");
+
+const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", ()  => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 })
+
+document.querySelector(".nav-link").forEach(n => n.addEventListener("click",  () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
